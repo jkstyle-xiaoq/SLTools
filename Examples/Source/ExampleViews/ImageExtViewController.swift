cb30d059284dc6b891d5a73ddd8c5e9d587a4189
jkstyle <jkstyle.xiaoq@gmail.com>
2018-06-14 11:02:46 +0800

    完善example

--- Examples/Source/ExampleViews/ImageExtViewController.swift
+++ Examples/Source/ExampleViews/ImageExtViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class ImageExtViewController: UIViewController {
 
     @IBOutlet weak var imageView1: UIImageView!
-    
     @IBOutlet weak var imageView2: UIImageView!
     
     @IBOutlet weak var imageView3: UIImageView!
-    
     @IBOutlet weak var imageView4: UIImageView!
-    
     @IBOutlet weak var imageView5: UIImageView!
+    
+    @IBOutlet weak var imageView6: UIImageView!
+    @IBOutlet weak var imageView7: UIImageView!
+    @IBOutlet weak var imageView8: UIImageView!
+    
+    @IBOutlet weak var imageView9: UIImageView!
+    @IBOutlet weak var imageView10: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +32,20 @@ class ImageExtViewController: UIViewController {
         self.imageView2.image = UIImage.sl_image(withColor: UIColor.blue, size: self.imageView2.bounds.size)
         
         // 创建边框图片(TBD 需要优化，fill和border颜色叠加了)
-        self.imageView3.image = UIImage.sl_image(fillColor: UIColor.gray, borderColor: UIColor.red, borderWidth: 1.0, size: self.imageView3.bounds.size, opaque: false, scale: 0.0, cornerRadius: 0.0)
-        self.imageView4.image = UIImage.sl_image(fillColor: UIColor.gray, borderColor: UIColor.red, borderWidth: 1.0, size: self.imageView4.bounds.size, opaque: false, scale: 0.0, cornerRadius: self.imageView4.bounds.height/2.0)
-        self.imageView5.image = UIImage.sl_image(fillColor: UIColor.clear, borderColor: UIColor.red, borderWidth: 2.0, size: self.imageView5.bounds.size, opaque: false, scale: 0.0, cornerRadius: 10.0)
+        self.imageView3.image = UIImage.sl_image(fillColor: UIColor.clear, borderColor: UIColor.red, borderWidth: 1.0, size: self.imageView3.bounds.size, opaque: false, scale: 0.0, cornerRadius: 0.0)
+        self.imageView4.image = UIImage.sl_image(fillColor: UIColor.clear, borderColor: UIColor.red, borderWidth: 4.0, size: self.imageView4.bounds.size, opaque: false, scale: 0.0, cornerRadius: self.imageView4.bounds.height/2.0)
+        self.imageView5.image = UIImage.sl_image(fillColor: UIColor.clear, borderColor: UIColor.red, borderWidth: 4.0, size: self.imageView5.bounds.size, opaque: false, scale: 0.0, cornerRadius: 10.0)
+        
+        // tint image
+        let deleteImage = #imageLiteral(resourceName: "delete")
+        let color = UIColor(RGBAValue: 0xFF0000FF)
+        self.imageView6.image = deleteImage.sl_tint(tintColor: color, mode: .sourceAtop)
+        self.imageView7.image = deleteImage.sl_tint(tintColor: UIColor.blue, mode: .sourceAtop)
+        self.imageView8.image = deleteImage.sl_tint(tintColor: UIColor.blue, mode: .sourceIn)
+        
+        // fill alpha
+        self.imageView9.image = deleteImage.sl_fillAlpha(fillColor: UIColor.blue)
+        self.imageView10.image = deleteImage.sl_fillAlpha(fillColor: UIColor.green)
     }
 
     override func didReceiveMemoryWarning() {
