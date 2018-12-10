2401b089396d21979a77a465e243f22d49b243a9
jkstyle <jkstyle.xiaoq@gmail.com>
2018-12-10 10:22:56 +0800

    String增加emoji检测，backspace接口

---
+++ Source/String+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  String+Extension.swift
+//  SLTools
+//
+//  Created by kentjia on 2018/12/8.
+//  Copyright © 2018 jkstyle. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    
+    func backspace() -> String {
+        return ""
+    }
+    
+    func backspacingSubstrings() -> [String] {
+        return []
+    }
+    
+    func isEmoji() -> Bool {
+        return false
+    }
+}
